1e3afc7874dfa937beebe565642cf6d12e70fafb
rosin23 <rosinessrosin@gmail.com>
2024-03-14 10:06:49 +0900

    init

---
+++ src/test/java/com/nhnacademy/store99/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.store99.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
